475f21108f45b0619e750891fbfcb17affddb59b
angularprac01 <namanagr1998@gmail.com>
2021-01-21 12:08:33 +0530

    all 20-01-2020

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import{ Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr strange' },
+    { id: 12, name: 'Narcos' },
+    { id: 13, name: 'pikacu' },
+    { id: 14, name: 'don' },
+    { id: 15, name: 'iron man' },
+    { id: 16, name: 'he man' },
+    { id: 17, name: 'Dynamo' },
+    { id: 18, name: 'Dr saluke' },
+    { id: 19, name: 'popi' },
+    { id: 20, name: 'rr' }
+];
